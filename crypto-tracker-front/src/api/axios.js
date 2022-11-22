a482b08da8b7ec4aba1bc40e7fadbfc90fe8974b
Joe Goble <Modulo216@users.noreply.github.com>
2022-11-22 08:31:55 -0500

    Env variables

--- crypto-tracker-front/src/api/axios.js
+++ crypto-tracker-front/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: `http://be:5001`,
+  baseURL: `http://${process.env.NODE_ENV === 'development' ? 'localhost' : '192.168.86.2'}:5001`,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
